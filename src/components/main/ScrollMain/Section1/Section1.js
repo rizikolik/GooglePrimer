7e8fc536099620fa56fdf52d6412f62d50aa3c0b
yunustr <ynsture@gmail.com>
2019-07-03 00:07:12 +0300

    added new page

--- src/components/main/ScrollMain/Section1/Section1.js
+++ src/components/main/ScrollMain/Section1/Section1.js
@@ -23,16 +23,19 @@ class Section1 extends React.Component{
           
             <div className="device-frame" style={{opacity: 1}}>
               <div className="device-wrapper" style={{marginTop: 194.329}}>
-                <img className="google-phone" src="https://www.yourprimer.com/assets/img/pixel-phone.png" style={{width:276, opacity: 1 }}/>
-                    
-                        <img className="inside-img" src="https://www.yourprimer.com/assets/img/slides/slide-1_website_lesson_en.jpg" style={{opacity: 1 }}/>
-                        
-                     </div>
-                </div>
-                <div className="foreground-layer">
+                 <div className="phone-wrap">
+                    <img className="google-phone" src="https://www.yourprimer.com/assets/img/pixel-phone.png" style={{width:276, opacity: 1 }}/>
+                   </div>
+                  <div className="phone-inside">
+                     <img className="inside-img" src="https://www.yourprimer.com/assets/img/slides/slide-1_website_lesson_en.jpg" style={{opacity: 1 }}/>
+
+                   </div>
+               </div>
+            </div>
+            <div className="foreground-layer">
                 <div className="copy-wrapper" style={{height: 900}}>
               
-                      <div className="" data-slide="0" style={{width:402, marginTop:279.5}}>
+                      <div className="texts-1" data-slide="0" style={{width:402, marginTop:279.5}}>
                         <h2 className="responsive-h2" style={{fontSize: 37}}>Bite-sized lessons to better your business</h2>
                           <p className="responsive-p" style={{fontSize: 22}}>Download the free Primer app and discover the fast and easy way to learn new business and digital marketing skills. You can take a lesson on business planning, management, and more whenever you have 5 minutes free during your busy day.</p>
                         </div>
